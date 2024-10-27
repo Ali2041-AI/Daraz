1e31ca412766fffd6435cabf16ad4ff66d0c9fbb
Ali <muhammadali.adnan21@gmail.com>
2024-10-27 13:17:21 +0500

    latest update

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -21,7 +21,7 @@ function Home(){
          <div className="search-area w-full font-notoSans mt-4">
             <div className="w-10/12 mx-auto px-[1px] py-[1px] border rounded-full border-[#FE4960] flex items-center justify-center overflow-hidden">
                <img src={searchIcon} className="w-6 mr-1" alt="" />
-               <input type="text" className="flex-1 outline-none border-none" placeholder="search.." value={search} onChange={e=>setSearch(e.target.value)} />
+               <input type="text" className="flex-1 outline-none border-none text-sm" placeholder="search.." value={search} onChange={e=>setSearch(e.target.value)} />
                <button onClick={handleSearch} className="bg-[#f85607] px-3 py-1 text-white rounded-full">Search</button>
             </div>
 
