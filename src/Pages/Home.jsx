c9b23e6cfd9381eae8df31f88309f95cc93924cc
Ali <muhammadali.adnan21@gmail.com>
2024-10-28 07:22:09 +0500

    latest

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import images from "../assets/Images";
+import Customslider from "../components/custom.slider";
+import imagesSlider from "../assets/ImageSlider";
 
 function Home(){
 
@@ -26,10 +28,17 @@ function Home(){
                <button onClick={handleSearch} className="bg-[#f85607] px-2 py-1 text-sm  text-white rounded-full">Search</button>
             </div>
          </div>
-         {/* All Products goes here  */}
-         <div className="min-h-screen">
-            <p>I am here</p>
+         {/* Image Slider  */}
+         <div className="min-h-screen w-full mt-[100px] rounded-lg  overflow-hidden ">
+            <div className="w-[81%] mx-auto max-w-[600px] rounded-lg overflow-hidden">
+                <Customslider>
+                    {imagesSlider.map((image,index)=>{
+                        return <img src={image.img} alt={image.imgAlt} key={index} />
+                    })}
 
+
+                </Customslider>
+            </div>
          </div>
          
          {/* Footer fixed at the bottom */}
@@ -37,7 +46,7 @@ function Home(){
          <div className="w-full fixed bottom-0 font-notoSans">
             <div className="w-[81%] mx-auto max-w-[600px] flex justify-between items-center">
             <div className="flex flex-col items-center">
-                <img src={images.darazLogo} className="w-6" alt="" srcset="" />
+                <img src={images.darazLogo} className="w-6" alt=""  />
                 <p className="text-[10px] opacity-70">Home</p>
 
             </div>
---
+++ src/assets/ImageSlider.js
@@ -0,0 +1,23 @@
+import slider1 from '../assets/slider1.jpg'
+import slider2 from '../assets/slider2.jpg'
+import slider3 from '../assets/slider3.jpg'
+import slider4 from '../assets/slider4.jpg'
+
+
+const imagesSlider=
+[
+{img:slider1,
+  imgAlt:'sliderImage1'  
+},
+{img:slider2,
+    imgAlt:'sliderImage2'  
+},
+{img:slider3,
+    imgAlt:'sliderImage3'  
+},
+{img:slider4,
+    imgAlt:'sliderImage4'  
+},
+]
+
+export default imagesSlider;
