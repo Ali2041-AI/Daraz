fc0b91e6f30f1736ee149733350aeadc19b163f8
Ali <muhammadali.adnan21@gmail.com>
2024-10-28 07:47:43 +0500

    lt

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -21,8 +21,8 @@ function Home(){
     return(
         <>
         {/* top search area fixex */}
-         <div className="search-area  z-50 bg-white w-full fixed top-0  font-notoSans pt-4">
-            <div className="w-[91%] mx-auto px-[2px] py-[2px] max-w-[600px] border-[0.5px] rounded-full border-[#FE4960] flex items-center justify-center overflow-hidden">
+         <div className="search-area  z-50 bg-[#FAF6FC] w-full fixed top-0  font-notoSans py-3">
+            <div className="w-[91%] mx-auto bg-white px-[2px] py-[2px] max-w-[600px] border-[0.7px] rounded-full border-[#FE4960] flex items-center justify-center overflow-hidden">
                <img src={images.searchIcon} className="w-6 mr-1" alt="" />
                <input type="text" className="flex-1 outline-none border-none text-sm" placeholder="search.." value={search} onChange={e=>setSearch(e.target.value)} />
                <button onClick={handleSearch} className="bg-[#f85607] px-2 py-1 text-sm  text-white rounded-full">Search</button>
